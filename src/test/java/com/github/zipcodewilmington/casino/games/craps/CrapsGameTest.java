d393933fc1735d31ca436d68efd9f84c78cb20aa
rjohnson0851 <ron21johnson@comcast.net>
2024-11-15 16:38:32 -0500

    Made test shells

---
+++ src/test/java/com/github/zipcodewilmington/casino/games/craps/CrapsGameTest.java
@@ -0,0 +1,72 @@
+package com.github.zipcodewilmington.casino.games.craps;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CrapsGameTest {
+
+
+
+
+
+    @Test
+    void testAddPlayer() {
+
+    }
+
+    @Test
+    void testRemovePlayer() {
+    }
+
+    @Test
+    void testPlayTurn() {
+
+    }
+
+    @Test
+    void testCheckWin() {
+
+    }
+
+    @Test
+    void testIsGameOver() {
+    }
+
+    @Test
+    void testGetRandom() {
+
+    }
+
+    @Test
+    void testGetBet() {
+
+    }
+
+    @Test
+    void testCanAfford() {
+
+    }
+
+    @Test
+    void testUpdateBalance() {
+
+    }
+
+    @Test
+    void testRollDice() {
+
+    }
+
+    @Test
+    void testRollInitialWin() {
+    }
+
+    @Test
+    void testSetPoint() {
+    }
+
+    @Test
+    void testIsWin() {
+    }
+}
