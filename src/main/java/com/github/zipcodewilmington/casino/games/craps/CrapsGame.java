478bc09d66c4e8f6b742c31be4a44d5c0aecf99b
rjohnson0851 <ron21johnson@comcast.net>
2024-11-16 14:44:22 -0500

    Did a few tests

--- src/main/java/com/github/zipcodewilmington/casino/games/craps/CrapsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/craps/CrapsGame.java
@@ -1,4 +1,32 @@
 package com.github.zipcodewilmington.casino.games.craps;
 
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+import java.util.List;
+
 public class CrapsGame {
+    private String players;
+
+    public void add(PlayerInterface mockPlayer) {
+    }
+
+    public List<PlayerInterface> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(String players) {
+        this.players = players;
+    }
+
+    public boolean isGameOver() {
+        return false;
+    }
+
+    public int getRandom() {
+        return 0;
+    }
+
+    public int getBet() {
+        return 0;
+    }
 }
--- src/test/java/com/github/zipcodewilmington/casino/games/craps/CrapsGameTest.java
+++ src/test/java/com/github/zipcodewilmington/casino/games/craps/CrapsGameTest.java
@@ -1,17 +1,26 @@
 package com.github.zipcodewilmington.casino.games.craps;
 
 
+import com.github.zipcodewilmington.casino.PlayerInterface;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class CrapsGameTest {
+        private CrapsGame crapsGame;
 
 
+   @BeforeEach
+   void setUp(){
+       crapsGame = new CrapsGame();
 
+   }
 
 
     @Test
     void testAddPlayer() {
+
 
     }
 
@@ -31,16 +40,21 @@ class CrapsGameTest {
 
     @Test
     void testIsGameOver() {
+        assertFalse(crapsGame.isGameOver(), "Game should not be over initially.");
     }
 
     @Test
     void testGetRandom() {
-
+        for (int i = 0; i < 100; i++) {
+            int random = crapsGame.getRandom();
+            assertTrue(random >= 1 && random <= 6, "Random dice roll should be between 1 and 6.");
+        }
     }
 
     @Test
     void testGetBet() {
-
+        int bet = crapsGame.getBet();
+        assertEquals(0, bet, "Initial bet should be 0.");
     }
 
     @Test
