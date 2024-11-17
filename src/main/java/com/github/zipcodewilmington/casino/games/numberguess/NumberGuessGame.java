4c760f0e3741f282e1338a41ce6374324ce7849b
Kayan Reid <kreid@Zipcoders-MBP.fios-router.home>
2024-11-17 11:04:56 -0500

    add test for the NumberGuess Game constructors

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -1,7 +1,34 @@
 package com.github.zipcodewilmington.casino.games.numberguess;
 
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by leon on 7/21/2020.
  */
 public class NumberGuessGame {
+   // private List<PlayerInterface> players = new ArrayList<>();
+    private int numberToGuess;
+    private int numberOfTries;
+    private Random randomNumber;
+
+    public NumberGuessGame(){
+        this(10, new Random());
+    }
+
+    public NumberGuessGame(int numberOfTries, Random randomNumber) {
+        this.numberOfTries = numberOfTries;
+        this.numberToGuess = randomNumber.nextInt(100) + 1;
+    }
+
+    public int getNumberOfTries() {
+        return numberOfTries;
+    }
+
+    public int getNumberToGuess() {
+        return numberToGuess;
+    }
 }
---
+++ src/test/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGameTest.java
@@ -0,0 +1,64 @@
+package com.github.zipcodewilmington.casino.games.numberguess;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MockRandomNumGenerator extends Random{
+    private int num;
+
+    public MockRandomNumGenerator(int num){
+        this.num = num;
+    }
+
+    @Override
+    public int nextInt(int bound){
+        return num - 1;
+    }
+}
+public class NumberGuessGameTest {
+private static final Logger LOGGER = Logger.getLogger(NumberGuessGameTest.class.getName());
+    @Test
+    public void testDefaultConstructor(){
+        //Given
+        int expectedNumberOfTries = 10;
+
+        //When
+        NumberGuessGame numGame = new NumberGuessGame();
+
+        //Then
+        int actualNumberOfTries = numGame.getNumberOfTries();
+
+        Assert.assertEquals(expectedNumberOfTries, actualNumberOfTries);
+
+    }
+
+    @Test
+    public void testConstructorWithNumberAndTries(){
+        //Given
+        //int numberToGuess =75;
+
+        //Random randomNum = new Random(12345);
+        MockRandomNumGenerator testRandom = new MockRandomNumGenerator(50);
+        int expectedNumberOfTries = 10;
+
+        //When
+        NumberGuessGame numGame = new NumberGuessGame(expectedNumberOfTries, testRandom);
+
+        //Then
+        int expectedNumberToGuess = 50;
+
+        LOGGER.info("Expected number guessed to be: "+ expectedNumberToGuess);
+        LOGGER.info("Actual number guessed: "+ numGame.getNumberToGuess());
+
+        assertEquals(expectedNumberToGuess, numGame.getNumberToGuess());
+        assertEquals(expectedNumberOfTries, numGame.getNumberOfTries());
+
+    }
+
+
+}
