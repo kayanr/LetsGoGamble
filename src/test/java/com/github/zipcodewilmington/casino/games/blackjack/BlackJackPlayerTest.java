d9d241ab6c675b52ba3eedfbe238f0fa4676948c
sgreloch <samuelgreloch@yahoo.com>
2024-11-17 13:31:30 -0500

    blackjackplayer more test

--- src/test/java/com/github/zipcodewilmington/casino/games/blackjack/BlackJackPlayerTest.java
+++ src/test/java/com/github/zipcodewilmington/casino/games/blackjack/BlackJackPlayerTest.java
@@ -3,6 +3,7 @@ package com.github.zipcodewilmington.casino.games.blackjack;
 import Mechanics.Card;
 import Mechanics.Deck;
 import org.junit.Test;
+import org.junit.runners.Suite;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -42,14 +43,74 @@ public class BlackJackPlayerTest {
     }
 
 
+    @Test
+
+    public void drawTest(){
+
+        BlackJackPlayer player = new BlackJackPlayer(null);
+        Deck deck = new Deck();
+        player.draw(deck.dealCard());
+        player.draw(deck.dealCard());
+        assertEquals(2,player.getNumberCards());
+
+    }
+
+@Test
+
+public void getHandTotalTest(){
+
+        BlackJackPlayer player = new BlackJackPlayer(null);
+        Deck deck = new Deck();
+        player.draw(deck.dealCard());
+        player.draw(deck.dealCard());
+        player.cardReveal(true);
+        assertEquals(21, player.getHandTotal());
+
+}
+
+@Test
+
+    public void getNumberofCards(){
+
+        BlackJackPlayer player = new BlackJackPlayer(null);
+
+        Deck deck = new Deck();
+
+        player.draw(deck.dealCard());
+
+        assertEquals(1,player.getNumberCards());
+
+}
+
+
+@Test
+
+    public void getHandTest(){
+        BlackJackPlayer player = new BlackJackPlayer(null);
+        Deck deck = new Deck();
+        player.draw(deck.dealCard());
+
+
+
+}
+
+
+@Test
+
+
+    public void emptyHandTest(){
+
+        BlackJackPlayer player = new BlackJackPlayer(null);
+        Card card = new Card(1,1);
+        player.draw(card);
+        assertEquals(1,player.getNumberCards());
+        player.emptyHand();
+        assertEquals(0,player.getNumberCards());
 
 
 
 
-
-
-
-
+}
 
 
 
