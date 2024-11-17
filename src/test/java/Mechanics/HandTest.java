75a274a9714ce8fe7365703adb4b35bbc24e6b13
DeeptiMishra@1512 <mishra.deeptimishra@gmail.com>
2024-11-17 09:15:26 -0500

    "Completed test and code for Hand Class and made a few changes to Deck test and card class"

--- src/test/java/Mechanics/HandTest.java
+++ src/test/java/Mechanics/HandTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class HandTest {
 
-    private Hand hand;
+    private Hand hand = new Hand();
     private Card card1;
     private Card card2;
 
@@ -16,18 +16,39 @@ public class HandTest {
     public void setUp(){
 
         hand = new Hand();
-        card1 = new Card(new String[]{"Spades"}, new String[]{"Ace"});
-        card2 = new Card(new String[]{"Hearts"}, new String[]{"King"});
+        card1 = new Card(0,0);
+        card2 = new Card(1,12);
 
 
 
     }
 
+
+    @Test
+    public void addCardToHandTest(){
+        hand.addCard(card1);
+
+        Assert.assertFalse(hand.isEmpty());
+
+
+    }
+
+
+    @Test
+    public void getCardsTest(){
+
+        hand.addCard(card2);
+
+        Assert.assertFalse(hand.getCards().isEmpty());
+
+    }
+
+
     @Test
 
     public void testEmptyHand(){
-
-        Assert.assertEquals(true, hand.isEmpty());
+        hand.clear();
+        Assert.assertTrue(hand.isEmpty());
 
     }
 
