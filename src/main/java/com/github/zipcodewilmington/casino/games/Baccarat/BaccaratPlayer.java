2671b123c6ac40697c27391dddf8512ecef48889
DeeptiMishra@1512 <mishra.deeptimishra@gmail.com>
2024-11-19 00:11:25 -0500

    "Completed code for Casino Baccarat Game"

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/Baccarat/BaccaratPlayer.java
@@ -0,0 +1,16 @@
+package com.github.zipcodewilmington.casino.games.Baccarat;
+
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+public class BaccaratPlayer implements PlayerInterface{
+        @Override
+        public CasinoAccount getArcadeAccount() {
+            return null;
+        }
+
+        @Override
+        public <SomeReturnType> SomeReturnType play() {
+            return null;
+        }
+    }
